6a4dc8c0f4fa664f20dcd9738e826d6051defa7c
Ray Lam <55645423+waimanlam2019@users.noreply.github.com>
2025-08-20 11:28:23 +0800

    fixed POST api endpoint

--- src/main/java/site/raylambytes/aijobmatcher/controller/AIJobMatcherController.java
+++ src/main/java/site/raylambytes/aijobmatcher/controller/AIJobMatcherController.java
@@ -1,11 +1,14 @@
 package site.raylambytes.aijobmatcher.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import site.raylambytes.aijobmatcher.AIJobMatcherService;
 import site.raylambytes.aijobmatcher.jpa.JobMatchingResultDTO;
+import site.raylambytes.aijobmatcher.scraper.SeleniumJobScraper;
 
 import java.util.List;
 
@@ -13,20 +16,24 @@ import java.util.List;
 @RequestMapping("/api")
 @CrossOrigin(origins = { "http://localhost:5173", "http://localhost:3000" })
 public class AIJobMatcherController {
+    private static final Logger logger = LoggerFactory.getLogger(AIJobMatcherController.class);// for demo pu
 
     @Autowired
     private AIJobMatcherService aiJobMatcherService;
     // GET /api/status
     @GetMapping("/matchingresults")
     public ResponseEntity<List<JobMatchingResultDTO>> getMatchingResults() {
+        logger.info("Received GET request for matching results");
         List<JobMatchingResultDTO> results = aiJobMatcherService.getAllJobMatchingResults();
         return ResponseEntity.ok(results);
     }
 
     // POST /api/jobmatch
     @PostMapping("/dojobmatching")
-    public ResponseEntity<String> doJobMatching(@RequestBody String requestBody) {
+    public ResponseEntity<String> doJobMatching() {
+        logger.info("Received POST request to start job matching");
         aiJobMatcherService.runMatching();
+        logger.info("Job matching process finished successfully");
         return ResponseEntity.ok("Received POST request. You handle it from here, boss.");
     }
 }
